45196266d50b8b8e19c0c3be62d363703162e775
enamulhaque7 <enamulhaquemb01@gmail.com>
2021-06-09 22:23:01 +0600

    updating

---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from './Home/Navbar/Navbar';
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default Home;
